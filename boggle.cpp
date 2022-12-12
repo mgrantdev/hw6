76b59ec215ddf75c2e6b60af9c6729e3d3dfdcb0
Matthew Grant <mgrant.xi@gmail.com>
2022-12-12 11:15:39 -0800

    added prefix tree check

--- boggle.cpp
+++ boggle.cpp
@@ -45,9 +45,9 @@ void printBoard(const std::vector<std::vector<char>> &board)
 	{
 		for (unsigned int j = 0; j < n; j++)
 		{
-			//std::cout << std::setw(2) << board[i][j];
+			std::cout << std::setw(2) << board[i][j];
 		}
-		//std::cout << std::endl;
+		std::cout << std::endl;
 	}
 }
 
@@ -76,20 +76,22 @@ std::pair<std::set<std::string>, std::set<std::string>> parseDict(std::string fn
 std::set<std::string> boggle(const std::set<std::string> &dict, const std::set<std::string> &prefix, std::vector<std::vector<char>> &board)
 {
 	std::set<std::string> result;
+	std::vector<std::vector<std::string>> prefixTrees;
 	for (unsigned int i = 0; i < board.size(); i++)
 	{
+		prefixTrees.resize(3);
 		for (unsigned int j = 0; j < board.size(); j++)
 		{
-			boggleHelper(dict, prefix, board, "", result, i, j, 0, 1);
-			boggleHelper(dict, prefix, board, "", result, i, j, 1, 0);
-			boggleHelper(dict, prefix, board, "", result, i, j, 1, 1);
+			boggleHelper(dict, prefix, prefixTrees[0], board, "", result, i, j, 0, 1);
+			boggleHelper(dict, prefix, prefixTrees[1], board, "", result, i, j, 1, 0);
+			boggleHelper(dict, prefix, prefixTrees[2], board, "", result, i, j, 1, 1);
 		}
 	}
 
 	return result;
 }
 
-bool boggleHelper(const std::set<std::string> &dict, const std::set<std::string> &prefix, std::vector<std::vector<char>> &board,
+bool boggleHelper(const std::set<std::string> &dict, const std::set<std::string> &prefix, std::vector<std::string> &prefixTree, std::vector<std::vector<char>> &board,
 				  std::string word, std::set<std::string> &result, unsigned int r, unsigned int c, int dr, int dc)
 {
 	// add your solution here!
@@ -98,21 +100,12 @@ bool boggleHelper(const std::set<std::string> &dict, const std::set<std::string>
 	std::string currentWord = word + letter; // build word
 
 	// word found
-	//std::cout << "current letter: " << letter << std::endl;
-	//std::cout << currentWord << std::endl;
+	std::cout << "------\ncurrent letter: " << letter << std::endl;
+	std::cout << currentWord << std::endl;
 	if (currentWord.length() > 1 && dict.find(currentWord) != dict.end())
 	{
 		// check for prefixes in results and remove
-		std::set<std::string> resultCpy = result;
-		if(currentWord.length() > 2) {
-			for(std::set<std::string>::iterator it = result.begin(); it != result.end(); ++it) {
-				if (currentWord.find(*it) != std::string::npos) {
-					resultCpy.erase(*it);
-				}
-			}
-		}
-		result = resultCpy;
-		result.insert(currentWord);
+		prefixTree.push_back(currentWord);
 	}
 
 	// stop if the prefix isn't valid
@@ -124,62 +117,64 @@ bool boggleHelper(const std::set<std::string> &dict, const std::set<std::string>
 	if (currentWord.length() == board.size())
 		return false;
 
-	// check all neighbor cubes if no direction specified
-	if (dr == 0 && dc == 0)
-	{
-		for (int vert = 0; vert <= 1; vert++)
-		{
-			for (int horiz = 0; horiz <= 1; horiz++)
-			{
-				//std::cout << "horizontal direction: " << horiz << std::endl;
-				//std::cout << "vertical direction: " << vert << std::endl;
-				// watch for grid borders and used cubes
-				if (r + vert >= 0 && r + vert < board.size() &&
-					c + horiz >= 0 && c + horiz < board[0].size() &&
-					board[r + vert][c + horiz] != '-' &&
-					!(horiz == 0 && vert == 0) &&
-					currentWord.length() < board.size()
-				)
-				{
-					board[r][c] = '-'; // used cube
-					//std::cout << "trying neighbors" << std::endl;
-					if (boggleHelper(dict, prefix, board, currentWord, result, r + vert, c, vert, horiz) || boggleHelper(dict, prefix, board, currentWord, result, r, c + horiz, vert, horiz) || boggleHelper(dict, prefix, board, currentWord, result, r + vert, c + horiz, vert, horiz))
-						return true;
-					board[r][c] = origL; // reset cube for different letter arrangement
-				}
-			}
-		}
+	if(currentWord.length() == 1) {
+		prefixTree.clear();
 	}
-	else if (dr != 0 && dc == 0)
+
+ 	if (dr != 0 && dc == 0)
 	{
-		// horizontal case
-		if (r + dr >= 0 && r + dr < board.size())
+		// vertical case
+		std::cout << "vert case" << std::endl;
+		std::cout << "horizontal direction: " << dc << std::endl;
+		std::cout << "vertical direction: " << dr << std::endl;
+		if (r + dr >= 0 && r + dr < board.size() && board[r + dr][c + dc] != '-')
 		{
-			if (boggleHelper(dict, prefix, board, currentWord, result, r + dr, c, dr, dc))
+			board[r][c] = '-'; // used cube
+			if (boggleHelper(dict, prefix, prefixTree, board, currentWord, result, r + dr, c, dr, dc))
+			{
+				std::cout << "prefix count: " << prefixTree.size() << std::endl;
 				return true;
+			}
 			board[r][c] = origL; // reset cube for different letter arrangement
 		}
 	}
 	else if (dc != 0 && dr == 0)
 	{
-		// vertical case
-		if (c + dc >= 0 && c + dc < board.size())
+		// horizontal case
+		std::cout << "horiz case" << std::endl;
+		std::cout << "horizontal direction: " << dc << std::endl;
+		std::cout << "vertical direction: " << dr << std::endl;
+		if (c + dc >= 0 && c + dc < board.size() && board[r + dr][c + dc] != '-')
 		{
-			if (boggleHelper(dict, prefix, board, currentWord, result, r, c + dc, dr, dc))
+			board[r][c] = '-'; // used cube
+			if (boggleHelper(dict, prefix, prefixTree, board, currentWord, result, r, c + dc, dr, dc))
+			{
+				std::cout << "prefix count: " << prefixTree.size() << std::endl;
 				return true;
+			}
 			board[r][c] = origL; // reset cube for different letter arrangement
 		}
 	}
-	else if(dc != 0 && dr != 0)
+	else if (dc != 0 && dr != 0)
 	{
 		// diagonal case
-		//std::cout << "diagonal case" << std::endl;
-		if (c + dc >= 0 && c + dc < board.size() && r + dr >= 0 && r + dr < board.size())
+		std::cout << "diagonal case" << std::endl;
+		std::cout << "horizontal direction: " << dr << std::endl;
+		std::cout << "vertical direction: " << dc << std::endl;
+		if (c + dc >= 0 && c + dc < board.size() && r + dr >= 0 && r + dr < board.size() && board[r + dr][c + dc] != '-')
 		{
-			if (boggleHelper(dict, prefix, board, currentWord, result, r + dr, c + dc, dr, dc))
+			board[r][c] = '-'; // used cube
+			if (boggleHelper(dict, prefix, prefixTree, board, currentWord, result, r + dr, c + dc, dr, dc))
+			{
+				std::cout << "prefix count: " << prefixTree.size() << std::endl;
 				return true;
+			}
 			board[r][c] = origL; // reset cube for different letter arrangement
 		}
+	}
+
+	if(prefixTree.size() > 0) {
+		result.insert(prefixTree.back());
 	}
 	return false;
 }
